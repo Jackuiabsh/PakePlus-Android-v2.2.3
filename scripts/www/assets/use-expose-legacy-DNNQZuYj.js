eea136fa89ca3ec8c1410e55b31538a545df55dd
Jackuiabsh <136717885+Jackuiabsh@users.noreply.github.com>
2026-04-23 18:39:59 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/use-expose-legacy-DNNQZuYj.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-2LcRMJKe.js","./index-legacy-DaOqSxAk.js"],function(e,t){"use strict";var n,s;return{setters:[e=>{n=e.j},e=>{s=e.Y}],execute:function(){e("u",function(e){const t=s();t&&n(t.proxy,e)})}}});
